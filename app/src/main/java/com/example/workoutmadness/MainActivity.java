e91a06d1126a14ec49bc09ca82042497ab1a0ed3
joshrap67 <ghosthippo67@gmail.com>
2019-05-24 21:59:54 -0400

    Added a popup that appears warning if in progress workout is in danger of being lost

--- app/src/main/java/com/example/workoutmadness/MainActivity.java
+++ app/src/main/java/com/example/workoutmadness/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.workoutmadness;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -9,8 +11,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +28,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     private TextView toolbarTitleTV;
+    private NavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); // removes the app title from the toolbar
         drawer = findViewById(R.id.drawer);
-        NavigationView nav = findViewById(R.id.nav_view);
+        nav = findViewById(R.id.nav_view);
         nav.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.nav_draw_open, R.string.nav_draw_close);
@@ -119,23 +129,96 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_current_workout:
                 if(currentFrag instanceof NewWorkoutFragment){
                     if(((NewWorkoutFragment) currentFrag).isModified()){
-                        Toast.makeText(this, "Creating workout was modified", Toast.LENGTH_SHORT).show();
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                        final AlertDialog alertDialog = alertDialogBuilder.create();
+                        final View popupView = getLayoutInflater().inflate(R.layout.quit_popup, null);
+                        Button confirmButton = popupView.findViewById(R.id.popupYes);
+                        confirmButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                goToCurrentWorkout();
+                                alertDialog.dismiss();
+                            }
+                        });
+                        Button quitButton = popupView.findViewById(R.id.popupNo);
+                        quitButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                nav.setCheckedItem(R.id.nav_new_workout); // since fragment didn't change, currently selected item is still new workout
+                                alertDialog.dismiss();
+                            }
+                        });
+                        alertDialog.setView(popupView);
+                        alertDialog.show();
+                    }
+                    else{
+                        goToCurrentWorkout();
                     }
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new WorkoutFragment(), "CURRENT_WORKOUT").commit();
                 break;
-            case R.id.nav_new_workout:
+
+            case R.id.nav_my_workouts:
                 if(currentFrag instanceof WorkoutFragment ){
                     if(((WorkoutFragment) currentFrag).isModified()){
                         Toast.makeText(this, "Workout was modified", Toast.LENGTH_SHORT).show();
                     }
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new NewWorkoutFragment(), "NEW_WORKOUT").commit();
+                else if(currentFrag instanceof NewWorkoutFragment){
+                    if(((NewWorkoutFragment) currentFrag).isModified()){
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                        final AlertDialog alertDialog = alertDialogBuilder.create();
+                        // TODO change this to the right view once i create the layout
+                        final View popupView = getLayoutInflater().inflate(R.layout.quit_popup, null);
+                        Button confirmButton = popupView.findViewById(R.id.popupYes);
+                        confirmButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                goToCurrentWorkout();
+                                alertDialog.dismiss();
+                            }
+                        });
+                        Button quitButton = popupView.findViewById(R.id.popupNo);
+
+                        quitButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                alertDialog.dismiss();
+                            }
+                        });
+                        alertDialog.setView(popupView);
+                        alertDialog.show();
+                    }
+                    else{
+                        goToCurrentWorkout();
+                    }
+                }
                 break;
+
+            case R.id.nav_new_workout:
+                if(currentFrag instanceof WorkoutFragment ){
+                    if(((WorkoutFragment) currentFrag).isModified()){
+                        // TODO save to file
+                        Toast.makeText(this, "Workout was modified", Toast.LENGTH_SHORT).show();
+                    }
+                    goToNewWorkout();
+                }
+                else if(!(currentFrag instanceof NewWorkoutFragment)) {
+                    goToNewWorkout();
+                }
+                break;
+
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void goToCurrentWorkout(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new WorkoutFragment(), "CURRENT_WORKOUT").commit();
+    }
+
+    public void goToNewWorkout(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new NewWorkoutFragment(), "NEW_WORKOUT").commit();
     }
 }
