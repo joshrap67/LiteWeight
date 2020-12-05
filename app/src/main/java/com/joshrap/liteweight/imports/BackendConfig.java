65980e4fed98a75e0f75dc5ca341469f4ec06340
joshrap67 <ghosthippo67@gmail.com>
2020-12-04 23:10:38 -0500

    Changed to prod

--- app/src/main/java/com/joshrap/liteweight/imports/BackendConfig.java
+++ app/src/main/java/com/joshrap/liteweight/imports/BackendConfig.java
@@ -2,7 +2,7 @@ package com.joshrap.liteweight.imports;
 
 public class BackendConfig {
     public static final String apiUrl = "https://yihza0d34f.execute-api.us-east-1.amazonaws.com/";
-    public static final String deploymentStage = "dev/";
+    public static final String deploymentStage = "prod/";
     public static final String cognitoClientId = "4ueb4e7af7cdnngqfdg0tfm9pk";
     public static final String s3ImageUrl ="https://liteweight-images.s3.us-east-1.amazonaws.com/";
     public static final String googleSignInClientId = "990471046455-g5s0mqhm6sm3b66ki7fle6n2ud0msim1.apps.googleusercontent.com";
