297ae26629c1d03221008910034e2f4d6f760015
Josh Rapoport <ghosthippo67@gmail.com>
2026-03-29 17:57:05 -0400

    Cleanup

--- app/src/main/java/com/joshrap/liteweight/activities/LandingActivity.java
+++ app/src/main/java/com/joshrap/liteweight/activities/LandingActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.joshrap.liteweight.R;
