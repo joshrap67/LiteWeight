a8adfc54e19c522d594baf591f237183f63efb25
Josh Rapoport <ghosthippo67@gmail.com>
2026-03-29 18:12:26 -0400

    wtf

--- app/src/main/java/com/joshrap/liteweight/fragments/ClockBottomFragment.java
+++ app/src/main/java/com/joshrap/liteweight/fragments/ClockBottomFragment.java
@@ -68,7 +68,7 @@ public class ClockBottomFragment extends BottomSheetDialogFragment {
         editor = sharedPreferences.edit();
 
         View view = inflater.inflate(R.layout.bottom_sheet_clock, container, false);
-        // get the views and attach the callbacks
+        // get the views and attach the listener
         stopwatchLayout = view.findViewById(R.id.stopwatch_layout);
         timerContainer = view.findViewById(R.id.timer_container);
 
